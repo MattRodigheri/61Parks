2c89d8a668980fc9a8ac6f0b6a90548e25508b69
MattRodigheri <mrodigheri@gmail.com>
2020-01-13 13:25:10 -0800

    header formatted

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,12 @@ import React from "react";
 import "../App.css";
 
 function Header() {
-  return <div className="Header">62Parks</div>;
+  return (
+    <div className="Header">
+      <h1>62Parks</h1>
+      <h3>About</h3>
+    </div>
+  );
 }
 
 export default Header;
